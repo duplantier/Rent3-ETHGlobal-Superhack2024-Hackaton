0c075e40ba5ff6e39da6e801bab18944c747ea64
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-08 18:08:16 +0300

    refactor: Move Worldcoin Verify button to the main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
-import Image from "next/image";
 import "../styles/globals.css";
 import DarkModeToggle from "@/components/DarkModeToggle";
 import Link from "next/link";
+import {
+  IDKitWidget,
+  VerificationLevel,
+  ISuccessResult,
+  useIDKit,
+} from "@worldcoin/idkit";
+import { useEffect, useState } from "react";
+/* import "dotenv/config"; */
+import Image from "next/image";
 import { useDarkMode } from "@/contexts/DarkModeContext";
-import SignInButton from "@/components/SignInButton";
-
 export default function App() {
   return (
     <main className="transition-all duration-300">
@@ -16,7 +22,35 @@ export default function App() {
 }
 
 const Navbar: React.FC = () => {
+  const [isUserVerifiedWithWorldID, setIsUserVerifiedWithWorldID] =
+    useState(false);
   const { darkMode } = useDarkMode();
+  const { open, setOpen } = useIDKit();
+  const [nullifierHash, setNullifierHash] = useState<string | null>(null);
+
+  const BACKEND_URI = process.env.NEXT_PUBLIC_BACKEND_URI;
+  // const APP_ID = process.env.NEXT_PUBLIC_WORLD_APP_ID as `app_${string}`;
+  // const ACTION_ID = "testing-action";
+  const handleVerify = async (proof: ISuccessResult) => {
+    const res = await fetch(`${BACKEND_URI}/world-id/verify`, {
+      // route to your backend will depend on implementation
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(proof),
+    });
+    setNullifierHash(proof.nullifier_hash);
+
+    if (!res.ok) {
+      throw new Error("Verification failed."); // IDKit will display the error message to the user in the modal
+    }
+  };
+
+  const onSuccess = (result: ISuccessResult) => {
+    console.log("onSuccess'a girdim");
+    console.log("nullifierHash", result.nullifier_hash);
+  };
 
   return (
     <section className="min-h-[10vh] transition-all duration-300 w-full fixed border-b-[1px] flex items-center justify-around bg-gray-50  text-gray-900 dark:text-gray-100 dark:bg-gray-900 dark:border-gray-700">
@@ -32,8 +66,44 @@ const Navbar: React.FC = () => {
         </h1>
       </div>
       <div className="flex justify-center items-center gap-4 font-semibold">
+        {nullifierHash !== null ? (
+          <button className="px-4 py-2 flex gap-2 justify-center items-center border-2 rounded-lg shadow-sm hover:bg-gray-100 hover:border-brand-black dark:border-gray-700 dark:bg-gray-800 dark:hover:border-brand-white">
+            ✅
+            <h1>
+              {nullifierHash.slice(0, 6)}...{nullifierHash.slice(-6)}
+            </h1>
+          </button>
+        ) : (
+          <IDKitWidget
+            app_id={"app_staging_cabc806110f5d7491c05482017dee619"}
+            action={"testing-action"}
+            onSuccess={onSuccess}
+            handleVerify={handleVerify}
+            verification_level={VerificationLevel.Device}
+          >
+            {({ open }) => (
+              // This is the button that will open the IDKit modal
+              <button
+                className="px-4 py-2 flex gap-2 justify-center items-center border-2 rounded-lg shadow-sm hover:bg-gray-100 hover:border-brand-black dark:border-gray-700 dark:bg-gray-800 dark:hover:border-brand-white"
+                onClick={open}
+              >
+                <Image
+                  src={`${
+                    darkMode
+                      ? `/logos/worldcoin-white.svg`
+                      : `/logos/worldcoin-black.svg`
+                  }`}
+                  alt="Logo"
+                  className="w-6 h-auto"
+                  width={20}
+                  height={40}
+                />
+                <h1>Sign In</h1>
+              </button>
+            )}
+          </IDKitWidget>
+        )}
         <DarkModeToggle />
-        <SignInButton />
       </div>
     </section>
   );
