5bde6b75860679b8956e08d6f7db99c0c4f1b0a6
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-09 19:03:36 +0300

    chore: remove backend folder

--- backend/src/world-id/dto/update-world-id.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateWorldIdDto } from './create-world-id.dto';
-
-export class UpdateWorldIdDto extends PartialType(CreateWorldIdDto) {}
--- backend/src/world-id/world-id.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WorldIdService } from './world-id.service';
-import { WorldIdController } from './world-id.controller';
-
-@Module({
-  controllers: [WorldIdController],
-  providers: [WorldIdService],
-})
-export class WorldIdModule {}
