1cdba1978f73bf6d19cd7cb880c1931b7e8c7578
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-11 16:20:57 +0300

    fix

--- frontend/src/app/dashboard/owner/page.tsx
+++ frontend/src/app/dashboard/owner/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Spotlight } from "@/components/ui/spotlight";
 import { useDarkMode } from "@/contexts/DarkModeContext";
-import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { z } from "zod";
@@ -24,7 +23,15 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import {
   DropdownMenuRadioGroup,
@@ -32,7 +39,6 @@ import {
 } from "@radix-ui/react-dropdown-menu";
 import { Check, Cross, Minus, Plus, X } from "lucide-react";
 import { ExpandableCard } from "@/components/ui/expendable-card";
-import { useWeb3Modal } from "@web3modal/wagmi/react";
 import { useAccount } from "wagmi";
 function getItemWithExpiry({ key }: { key: string }) {
   const itemStr = localStorage.getItem(key);
@@ -77,53 +83,6 @@ const OwnerPage = () => {
   const userNullifierHash = getItemWithExpiry({ key: "nullifier_hash" });
   const { darkMode } = useDarkMode();
   const userRole = getItemWithExpiry({ key: "userRole" });
-  if (
-    userNullifierHash == "" ||
-    userNullifierHash == null ||
-    userRole != "owners"
-  ) {
-    return (
-      <main className="min-h-[100vh] w-full dark:bg-brand-black  flex flex-col justify-center items-center bg-grid-small-black/[0.2] dark:bg-dot-white/[0.1] relative overflow-hidden">
-        <Spotlight
-          className="-top-40 left-0 md:left-0 md:-bottom-80"
-          fill="#27F2CD"
-        />
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <Link
-          href="/"
-          className="flex justify-center items-center gap-2 mb-6 z-10"
-        >
-          <Image
-            src={`${darkMode ? `/logo-white.svg` : `/logo-black.svg`}`}
-            alt="Logo"
-            className="w-10 rotate-90 h-auto"
-            width={20}
-            height={40}
-          />
-          <h1 className="tracking-tight font-black md:text-2xl lg:text-3xl dark:text-brand-white">
-            Rent3
-          </h1>
-        </Link>
-        <div className="flex gap-2 py-2 px-4 mb-6 z-50 items-center rounded-lg border bg-red-100 text-red-500 border-red-400 text-md ">
-          <Image
-            src={`/logos/worldcoin-black.svg`}
-            alt="Logo"
-            className="w-6 h-auto"
-            width={20}
-            height={40}
-          />
-          <div>You are not authorized to view this page</div>
-        </div>
-        <Link
-          href="/"
-          className="w-[200px]  px-4 py-2 flex gap-2 justify-center items-center border-2 rounded-lg shadow-sm bg-gray-900 border-gray-400 hover:border-brand-primary  dark:hover:bg-gray-950  text-gray-50 dark:text-slate-300 dark:hover:text-brand-white dark:hover:border-brand-primary hover:text-brand-primary"
-        >
-          Go Back
-        </Link>
-      </main>
-    );
-  }
-
   const BACKEND_URI = "https://rent3-backend.onrender.com/";
   const { address: userWalletAddress, isConnected } = useAccount();
   const [warning, setWarning] = useState("");
@@ -189,16 +148,65 @@ const OwnerPage = () => {
       setWarning("Failed to add property");
     }
   }
+  if (
+    userNullifierHash == "" ||
+    userNullifierHash == null ||
+    userRole != "owners"
+  ) {
+    return (
+      <main className="min-h-[100vh] w-full dark:bg-brand-black  flex flex-col justify-center items-center bg-grid-small-black/[0.2] dark:bg-dot-white/[0.1] relative overflow-hidden">
+        <Spotlight
+          className="-top-40 left-0 md:left-0 md:-bottom-80"
+          fill="#27F2CD"
+        />
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <Link
+          href="/"
+          className="flex justify-center items-center gap-2 mb-6 z-10"
+        >
+          <Image
+            src={`${darkMode ? `/logo-white.svg` : `/logo-black.svg`}`}
+            alt="Logo"
+            className="w-10 rotate-90 h-auto"
+            width={20}
+            height={40}
+          />
+          <h1 className="tracking-tight font-black md:text-2xl lg:text-3xl dark:text-brand-white">
+            Rent3
+          </h1>
+        </Link>
+        <div className="flex gap-2 py-2 px-4 mb-6 z-50 items-center rounded-lg border bg-red-100 text-red-500 border-red-400 text-md ">
+          <Image
+            src={`/logos/worldcoin-black.svg`}
+            alt="Logo"
+            className="w-6 h-auto"
+            width={20}
+            height={40}
+          />
+          <div>You are not authorized to view this page</div>
+        </div>
+        <Link
+          href="/"
+          className="w-[200px]  px-4 py-2 flex gap-2 justify-center items-center border-2 rounded-lg shadow-sm bg-gray-900 border-gray-400 hover:border-brand-primary  dark:hover:bg-gray-950  text-gray-50 dark:text-slate-300 dark:hover:text-brand-white dark:hover:border-brand-primary hover:text-brand-primary"
+        >
+          Go Back
+        </Link>
+      </main>
+    );
+  }
 
   useEffect(() => {
-    const fetchProperties = async () => {
+    const fetchOwnerProperties = async () => {
       try {
-        const response = await fetch(`${BACKEND_URI}property`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          `${BACKEND_URI}owners/${userNullifierHash}/properties`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
         const data = await response.json();
         setOwnerProperties(data);
       } catch (error) {
@@ -206,7 +214,7 @@ const OwnerPage = () => {
       }
     };
     if (ownerProperties.length === 0) {
-      fetchProperties();
+      fetchOwnerProperties();
     }
   }, []);
   return (
@@ -249,13 +257,128 @@ const OwnerPage = () => {
       </h1>
       <div className="w-[80vw] mx-auto flex justify-center items-center gap-20 z-50">
         <section
-          className="w-1/2 dark:bg-gray-900 border-2 dark:border-gray-700 flex flex-col px-4 py-6 rounded-lg"
+          className="w-1/2 bg-gray-50 dark:bg-gray-900 border-2 dark:border-gray-700 flex flex-col px-4 py-6 rounded-lg"
           id="listedProperties"
         >
           <h1 className="text-3xl font-bold text-brand-black dark:text-brand-white">
             Listed Properties
           </h1>
-          <div className="w-full h-max-[400px] overflow-y-scroll"></div>
+          <section className="z-50 bg-gray-50 dark:bg-gray-900">
+            <div className="flex w-[90%] mx-auto flex-wrap justify-center items-center gap-10 bg-gray-50 dark:bg-gray-900">
+              {ownerProperties.length > 0 ? (
+                ownerProperties.map(
+                  (property: {
+                    id: string;
+                    ownerAddress: string;
+                    tenantAddress: string;
+                    country: string;
+                    city: string;
+                    neighborhood: string;
+                    type: string;
+                    bedrooms: number;
+                    bathrooms: number;
+                    furnished: boolean;
+                    floorSizeSqm: number;
+                    description: string;
+                    images: string[];
+                    rentalPrice: number;
+                  }) => (
+                    <Dialog key={property.id}>
+                      <DialogTrigger className="w-[300px] bg-gray-50 dark:bg-gray-900 dark:border-gray-700 h-auto py-6 mx-auto flex flex-col justify-center items-center dark:text-brand-white text-brand-black border border-gray-700 px-2 my-4 rounded-lg">
+                        <Image
+                          src={property.images[0]}
+                          alt="Property Image"
+                          width={200}
+                          height={200}
+                          className="w-auto h-auto rounded-lg"
+                        />
+                        <div className="flex flex-col gap-2">
+                          <h2 className="mt-4 font-bold text-xl">
+                            {property.city} ({property.country})
+                          </h2>
+                          <h3>{property.rentalPrice}</h3>
+                        </div>
+                        <button className="w-[80%] mx-auto mt-4  px-4 py-2 flex gap-2 justify-center items-center border-2 rounded-lg shadow-sm bg-gray-900 border-gray-400 hover:border-brand-primary  dark:hover:bg-gray-950  text-gray-50 dark:text-slate-300 dark:hover:text-brand-white dark:hover:border-brand-primary hover:text-brand-primary">
+                          Details
+                        </button>
+                      </DialogTrigger>
+                      <DialogContent className="text-brand-black dark:text-brand-white">
+                        <DialogHeader>
+                          <DialogTitle>
+                            {property.city} ({property.country})
+                          </DialogTitle>
+                          <DialogDescription>
+                            <div className="flex flex-col gap-3">
+                              <p>{property.description}</p>
+                              <div className="flex gap-2 w-full overflow-x-scroll my-4">
+                                {property.images.map((image: string) => (
+                                  <Image
+                                    key={image}
+                                    src={image}
+                                    alt="Property Image"
+                                    width={200}
+                                    height={200}
+                                    className="w-auto h-auto rounded-lg"
+                                  />
+                                ))}
+                              </div>
+
+                              <p>
+                                <span className="font-bold">Type:</span>{" "}
+                                {property.type}
+                              </p>
+
+                              <p>
+                                <span className="font-bold">Type:</span>{" "}
+                                {property.neighborhood}
+                              </p>
+                              <p>
+                                <span className="font-bold">Bedrooms:</span>{" "}
+                                {property.bedrooms}
+                              </p>
+
+                              <p>
+                                <span className="font-bold">Bathrooms:</span>{" "}
+                                {property.bathrooms}
+                              </p>
+
+                              <p>
+                                <span className="font-bold">Furnished:</span>{" "}
+                                {property.furnished ? "Yes" : "No"}
+                              </p>
+                              <p>
+                                <span className="font-bold">Floor Size:</span>{" "}
+                                {property.floorSizeSqm} sqm
+                              </p>
+
+                              <p>
+                                <span className="font-bold">Rental Price:</span>{" "}
+                                {property.rentalPrice}
+                              </p>
+                              <button className="w-[80%] mx-auto mt-4  px-4 py-2 flex gap-2 justify-center items-center border-2 rounded-lg shadow-sm bg-gray-900 border-gray-400 hover:border-brand-primary  dark:hover:bg-gray-950  text-gray-50 dark:text-slate-300 dark:hover:text-brand-white dark:hover:border-brand-primary hover:text-brand-primary">
+                                Rent Now
+                              </button>
+                            </div>
+                          </DialogDescription>
+                        </DialogHeader>
+                      </DialogContent>
+                    </Dialog>
+                  )
+                )
+              ) : (
+                <div className="flex my-6 flex-col justify-center items-center gap-4">
+                  <Image
+                    src="/void.svg"
+                    width={200}
+                    height={200}
+                    alt="Void"
+                    className="h-auto "
+                  />
+                  <h1 className="font-bold text-md">No property found</h1>
+                </div>
+              )}
+            </div>
+          </section>
         </section>
         <section
           className="w-1/2 bg-gray-50 dark:bg-gray-900 border-2 dark:border-gray-700 flex flex-col  px-4 py-6 rounded-lg"
