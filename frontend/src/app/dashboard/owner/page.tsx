bb34ed5935e24cb97c8540764ddda7271fc2e51a
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-11 16:28:24 +0300

    fix

--- frontend/src/app/dashboard/owner/page.tsx
+++ frontend/src/app/dashboard/owner/page.tsx
@@ -148,52 +148,6 @@ const OwnerPage = () => {
       setWarning("Failed to add property");
     }
   }
-  if (
-    userNullifierHash == "" ||
-    userNullifierHash == null ||
-    userRole != "owners"
-  ) {
-    return (
-      <main className="min-h-[100vh] w-full dark:bg-brand-black  flex flex-col justify-center items-center bg-grid-small-black/[0.2] dark:bg-dot-white/[0.1] relative overflow-hidden">
-        <Spotlight
-          className="-top-40 left-0 md:left-0 md:-bottom-80"
-          fill="#27F2CD"
-        />
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <Link
-          href="/"
-          className="flex justify-center items-center gap-2 mb-6 z-10"
-        >
-          <Image
-            src={`${darkMode ? `/logo-white.svg` : `/logo-black.svg`}`}
-            alt="Logo"
-            className="w-10 rotate-90 h-auto"
-            width={20}
-            height={40}
-          />
-          <h1 className="tracking-tight font-black md:text-2xl lg:text-3xl dark:text-brand-white">
-            Rent3
-          </h1>
-        </Link>
-        <div className="flex gap-2 py-2 px-4 mb-6 z-50 items-center rounded-lg border bg-red-100 text-red-500 border-red-400 text-md ">
-          <Image
-            src={`/logos/worldcoin-black.svg`}
-            alt="Logo"
-            className="w-6 h-auto"
-            width={20}
-            height={40}
-          />
-          <div>You are not authorized to view this page</div>
-        </div>
-        <Link
-          href="/"
-          className="w-[200px]  px-4 py-2 flex gap-2 justify-center items-center border-2 rounded-lg shadow-sm bg-gray-900 border-gray-400 hover:border-brand-primary  dark:hover:bg-gray-950  text-gray-50 dark:text-slate-300 dark:hover:text-brand-white dark:hover:border-brand-primary hover:text-brand-primary"
-        >
-          Go Back
-        </Link>
-      </main>
-    );
-  }
 
   useEffect(() => {
     const fetchOwnerProperties = async () => {
@@ -217,7 +171,48 @@ const OwnerPage = () => {
       fetchOwnerProperties();
     }
   }, []);
-  return (
+  return userNullifierHash == "" ||
+    userNullifierHash == null ||
+    userRole != "owners" ? (
+    <main className="min-h-[100vh] w-full dark:bg-brand-black  flex flex-col justify-center items-center bg-grid-small-black/[0.2] dark:bg-dot-white/[0.1] relative overflow-hidden">
+      <Spotlight
+        className="-top-40 left-0 md:left-0 md:-bottom-80"
+        fill="#27F2CD"
+      />
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <Link
+        href="/"
+        className="flex justify-center items-center gap-2 mb-6 z-10"
+      >
+        <Image
+          src={`${darkMode ? `/logo-white.svg` : `/logo-black.svg`}`}
+          alt="Logo"
+          className="w-10 rotate-90 h-auto"
+          width={20}
+          height={40}
+        />
+        <h1 className="tracking-tight font-black md:text-2xl lg:text-3xl dark:text-brand-white">
+          Rent3
+        </h1>
+      </Link>
+      <div className="flex gap-2 py-2 px-4 mb-6 z-50 items-center rounded-lg border bg-red-100 text-red-500 border-red-400 text-md ">
+        <Image
+          src={`/logos/worldcoin-black.svg`}
+          alt="Logo"
+          className="w-6 h-auto"
+          width={20}
+          height={40}
+        />
+        <div>You are not authorized to view this page</div>
+      </div>
+      <Link
+        href="/"
+        className="w-[200px]  px-4 py-2 flex gap-2 justify-center items-center border-2 rounded-lg shadow-sm bg-gray-900 border-gray-400 hover:border-brand-primary  dark:hover:bg-gray-950  text-gray-50 dark:text-slate-300 dark:hover:text-brand-white dark:hover:border-brand-primary hover:text-brand-primary"
+      >
+        Go Back
+      </Link>
+    </main>
+  ) : (
     <main className="min-h-[100vh] overflow-y-scoll py-12 w-full bg-brand-white dark:bg-brand-black  flex flex-col justify-center items-center bg-dot-black/[0.5] dark:bg-dot-white/[0.1] relative ">
       <Spotlight
         className="-top-40 left-0 md:left-0 md:-bottom-80"
